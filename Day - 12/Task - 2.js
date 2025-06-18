2156aabe296302144f86f2df7f320b8de1bac386
Elson18 <elsonaron54@gmail.com>
2025-06-18 09:10:23 +0530

    Day - 12

---
+++ Day - 12/Task - 2.js
@@ -0,0 +1,3 @@
+let a=prompt("enter")
+let d=a.length;
+alert("you have used: "+d+" Remaining: "+(30-d) + " Char")
---
+++ Day - 12/Task - 3.js
@@ -0,0 +1,4 @@
+let name = "Elson Benanzal A"
+let age = 19
+let isstudent = true
+console.log("My name is " + name+", I am "+age+" years old. student :"+isstudent);
